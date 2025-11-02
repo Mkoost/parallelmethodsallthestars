8304e6e8dcc4c4f1d9b8dce59d1f42c2e5b249d1
mkoost <mischa.kagan@yandex.ru>
2025-11-02 18:43:54 +0300

    WAT IS ZHAT

--- Nbodyproblem/newmain.cpp
+++ Nbodyproblem/newmain.cpp
@@ -2,15 +2,15 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <iterator>
 #include <omp.h>
+#include <ostream>
 #include <string>
 #include <vector>
 
 using vec3 = double[3];
 using vec2 = double[2];
 
-constexpr const double G = 6.67 * 1e-11;
+constexpr const double G = 6.67e-11;
 constexpr const vec3 zerovec{0, 0, 0};
 
 struct NBodyProblem {
@@ -24,34 +24,33 @@ struct NBodyProblem {
   };
 
   std::vector<Body> bodies;
-  int n;
-  NBodyProblem(int n_) : bodies(n_), n(n_) {}
+  NBodyProblem(int n_) : bodies(n_) {}
 
   void calculate_a() {
+    int n = bodies.size();
 #pragma omp for
     for (int i = 0; i < n; ++i) {
       Body b = bodies[i];
       b.a[0] = b.a[1] = b.a[2] = 0.;
-
-      for (int j = 0; j < n; ++i) {
+      for (int j = 0; j < n; ++j) {
         if (i == j)
           continue;
 
         vec3 r;
         r[0] = b.r[0] - bodies[j].r[0];
-        r[1] = b.r[0] - bodies[j].r[0];
-        r[2] = b.r[0] - bodies[j].r[0];
+        r[1] = b.r[1] - bodies[j].r[1];
+        r[2] = b.r[2] - bodies[j].r[2];
 
         double r3 = r[0] * r[0] + r[1] * r[1] + r[2] * r[2];
         r3 *= std::sqrt(r3);
-        r3 = std::max(r3, 1e-10);
+        r3 = std::max(r3, 1e-30);
 
         for (int k = 0; k < 3; ++k)
-          b.a[k] -= r[k] * bodies[j].mass / r3;
+          b.a[k] -= (G * bodies[j].mass) * r[k] / r3;
       }
 
       for (int k = 0; k < 3; ++k)
-        bodies[i].a[k] = G * b.a[k];
+        bodies[i].a[k] = b.a[k];
     }
   }
 };
@@ -69,15 +68,17 @@ struct RK4Integrator {
 
   std::vector<RKdata> yn;
 
-  RK4Integrator(int n_, double tau_) : tau(tau_), yn(n_), n(n_) {}
+  RK4Integrator(int n_, double tau_) : tau(tau_), yn(n_) {}
 
   void step(double num, NBodyProblem &nbodies) {
-#pragma omp parallel num_threads(                                              \
-        omp_get_max_threads()) if (omp_get_max_threads() > 1)
+
+    int n = yn.size();
+#pragma omp parallel shared(num, n, nbodies)                                                 \
+    num_threads(omp_get_max_threads()) if (omp_get_max_threads() > 1)
     {
       for (int ii = 0; ii < num; ++ii) {
-        nbodies.calculate_a();
 
+        nbodies.calculate_a();
 #pragma omp single
         for (int i = 0; i < n; ++i) {
           for (int k = 0; k < 3; ++k) {
@@ -143,9 +144,12 @@ struct Interface {
   NBodyProblem nbodies;
   RK4Integrator integrator;
   int n;
-  Interface(int n_, double tau_) : nbodies(n_), integrator(n_, tau_), n(n_) {};
+  double t;
+  Interface(int n_, double tau_)
+      : nbodies(n_), integrator(n_, tau_), n(n_), t(0) {};
 
-  Interface(std::string &path,  double tau_) : nbodies(0), integrator(0, tau_), n(0) {
+  Interface(const std::string &path, double tau_)
+      : nbodies(0), integrator(0, tau_), n(0), t(0) {
 
     std::ifstream fin(path);
 
@@ -162,7 +166,10 @@ struct Interface {
     fin.close();
   };
 
-  void step(double num) { integrator.step(n, nbodies); }
+  void step(double num) {
+    t += num * integrator.tau;
+    integrator.step(num, nbodies);
+  }
 
   void reserve(int num) {
     integrator.yn.reserve(num);
@@ -174,14 +181,26 @@ struct Interface {
     nbodies.bodies.resize(num);
   }
 
-
   template <class U> void save_state(const U &path) {
     std::ofstream outFile(path, std::ios::app);
 
     for (size_t i = 0; i < n; ++i)
-      outFile << nbodies.bodies[i].r[0] << nbodies.bodies[i].r[1]
-              << nbodies.bodies[i].r[2];
+      outFile << std::setprecision(14) << nbodies.bodies[i].r[0] << " "
+              << nbodies.bodies[i].r[1] << " " << nbodies.bodies[i].r[2]
+              << std::endl;
 
     outFile.close();
   }
 };
+
+int main() {
+  double tau = 0.01;
+  std::cout << "MY N-BODY PROBLEM" << std::endl;
+  Interface nbod("4body.txt", tau);
+  double t1 = -omp_get_wtime();
+  omp_set_num_threads(1);
+  nbod.step(int(20. / tau));
+  t1 += omp_get_wtime();
+  std::cout << t1 << std::endl;
+  nbod.save_state("res.txt");
+}
