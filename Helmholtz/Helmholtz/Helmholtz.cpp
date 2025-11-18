c5a63d5efa9e177ffc6f390849eb0f6b062de76a
mrboobrik <ilyak150@gmail.com>
2025-11-18 15:19:14 +0300

    jacobytest

--- Helmholtz/Helmholtz/Helmholtz.cpp
+++ Helmholtz/Helmholtz/Helmholtz.cpp
@@ -18,7 +18,12 @@ struct HelmholtzGridScheme {
     HelmholtzGridScheme(int n_, double k_) : h(1. / (n_ - 1)), grid(n_* n_, 0), n(n_), k(k_) {}
 
     double& operator()(int i, int j) { return grid[i * n + j]; }
-    void approximate_node(int i, int j) { grid[i * n + j] = (h * h * f(h * j, h * i, k) + grid[i * n + (j - 1)] + grid[i * n + (j + 1)] + grid[(i - 1) * n + j] + grid[(i + 1) * n + j]) / (4. + k * k * h * h); }
+    
+    void approximate_node(int i, int j) { 
+        grid[i * n + j] = (h * h * f(h * j, h * i, k) + grid[i * n + (j - 1)] + grid[i * n + (j + 1)] + grid[(i - 1) * n + j] + grid[(i + 1) * n + j]) / (4. + k * k * h * h); }
+    
+    void approximate_node(int i, int j, std::vector<double>& vec) { 
+        grid[i * n + j] = (h * h * f(h * j, h * i, k) + vec[i * n + (j - 1)] + vec[i * n + (j + 1)] + vec[(i - 1) * n + j] + vec[(i + 1) * n + j]) / (4. + k * k * h * h); }
 
     void print() {
         for (int i = 0; i < n; ++i)
@@ -27,23 +32,56 @@ struct HelmholtzGridScheme {
                 std::cout << grid[i * n + j] << " ";
             std::cout << std::endl;
         }
-
     }
 };
 
 
 struct JacobyIteration {
-    double err = 0;
-    /*Какие-то еще переменные*/
-    void step(HelmholtzGridScheme& matrix);
+    double err = 0.0;
+    std::vector<double> old;
+    JacobyIteration(int n): old(n*n) {};
+    double get_err() const { return err; }
 
+    void step(HelmholtzGridScheme& matrix) {
+        const int n = matrix.n;
+        double x;
+        double y;        
+        if (n <= 2) return;
+#pragma omp single
+
+        old.swap(matrix.grid);  // КЛЮЧЕВАЯ СТРОКА
+
+        const double h2 = matrix.h * matrix.h;
+        const double denom = 4.0 + matrix.k * matrix.k * h2;
+
+        double local_err = 0.0;
+        double err_n = err / omp_get_num_threads();
+
+#pragma omp for nowait
+            for (int i = 1; i < n-1; ++i) {
+                for (int j = 1; j < n-1; ++j) {
+                    matrix.approximate_node(i, j, old);
+                    local_err += std::abs(matrix(i, j) - old[i*n + j]);
+   
+                }
+            }
+        
+        
+        #pragma omp critical 
+        {
+            err += local_err / n - err_n;
+        }
+
+#pragma omp barrier
+    }
 };
+
 
 class RedNBlackIterations {
     double err = 0;
 public:
     double get_err() { return err; };
-
+    RedNBlackIterations(int n){}
     void step(HelmholtzGridScheme& matrix) {
         int n = matrix.n;
 
@@ -90,7 +128,7 @@ class Interface{
     HelmholtzGridScheme matrix;
     Solver solver;
 public:
-    Interface(int n, double k) : matrix(n, k) {};
+    Interface(int n, double k) : matrix(n, k), solver(n) {};
     int solve(double err) {
         int i = 0;
 #pragma omp parallel shared(matrix, solver, i)  num_threads(omp_get_max_threads()) if (omp_get_max_threads() > 1)
@@ -112,18 +150,15 @@ public:
     /*Какие-то еще доп. функции*/
 };
 
-
-
-int main()
-{
-    int n = 10000;
+template<class Solver>
+void time_test(int n = 1000){
     double h = 1. / (n-1);
-    Interface<RedNBlackIterations> interface(n, n / (h * h));
+    Interface<Solver> interface(n, 1/(h));
     double t = -omp_get_wtime();
-    int iter_num = interface.solve(0.01);
+    int iter_num = interface.solve(0.00001);
     t += omp_get_wtime();
 
-    std::cout << "error: " << interface.get_err() << " iteration num: " << iter_num << "\n\n";
+    std::cout << "Error: " << interface.get_err() << " iteration num: " << iter_num << "\n";
     //interface.print();
 
     double realErr = 0;
@@ -131,7 +166,17 @@ int main()
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
             realErr += std::fabs(interface.get_node(i, j) - u(j * h, i * h));
-    std::cout << "Real error: " << realErr / n << " time: " << t << "s";
+    std::cout << "Real error: " << realErr / n << " time: " << t << "s" << std::endl;
+}
+
+int main()
+{
+    std::cout << "RedNBlackIterations" << std::endl;
+    time_test<RedNBlackIterations>();
+
+    std::cout << std::endl << "JacobyIteration" << std::endl;
+    time_test<JacobyIteration>();
+    std::cout << std::endl;
 
 }
 
